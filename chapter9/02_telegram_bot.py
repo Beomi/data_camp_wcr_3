557645c83866c677f5232f497cf2f51fca18c381
beomi <latheledusjp@gmail.com>
2018-03-24 12:10:09 +0900

    telegram bot

---
+++ chapter9/02_telegram_bot.py
@@ -0,0 +1,5 @@
+import telegram
+
+bot = telegram.Bot(token='Telegram:botToken')
+chat_id = bot.getUpdates()[-1].message.chat.id
+bot.sendMessage(chat_id=chat_id, text=chat_id)
---
+++ chapter9/03_naver_1min.py
@@ -0,0 +1,18 @@
+import telegram
+import time
+import requests
+from bs4 import BeautifulSoup as bs
+
+bot = telegram.Bot(token='Telegram:botToken')
+
+while True:
+    res = requests.get('http://naver.com')
+    soup = bs(res.text, 'lxml')
+    top_list = soup.select('.PM_CL_realtimeKeyword_rolling span.ah_k')
+    result = ''
+    for top in top_list:
+        result += top.text
+    bot.sendMessage(chat_id='17261014', text=result)
+
+    time.sleep(30)
+
